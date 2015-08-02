c91f15340843db17542d33b97000c4f0ba5584fe
clemencevaz <clemencevaz@gmail.com>
2015-08-02 11:25:34 -0300

    Update IA02.pl

--- IA02.pl
+++ IA02.pl
@@ -1,6 +1,6 @@
 
-/*____________________________MENU DE DEPART___________________________ */
-liredepart:-nl, write('1. Partie Ã  2 joueurs'),nl,
+/*____________________________MAIN MENU___________________________ */
+liredepart:-nl, write('1. Partie ÃƒÂ  2 joueurs'),nl,
 write('2. Partie contre un bot'),nl,
 write('3. Partie bot contre bot'),nl,
 write('4. Quitter'),nl,
@@ -14,7 +14,7 @@ appel(3):- plateau_depart(P), jeuOrdi(P, 1),!.
 appel(4):-write('Au revoir!'), abort.
 
 %jeuOrdi(+Plateau, +Joueur) 
-%Ce prédicat est utilisé lorsque deux IA s'affrontent
+%Game between 2 bots
 jeuOrdi([Marche,Bourse,Trader,ResJ1,ResJ2],JoueurenCours) :-
 	length(Marche, Res),
 	(Res > 2 -> 
@@ -22,14 +22,14 @@ jeuOrdi([Marche,Bourse,Trader,ResJ1,ResJ2],JoueurenCours) :-
 		jeuOrdi(NewP, NewJoueur); gagnant([Marche,Bourse,Trader,ResJ1,ResJ2], G), nl, write('Le gagnant est le joueur '), write(G)).
 
 %jeuHumainHumain(+Plateau, +Joueur)
-%Ce prédicat est utilisé lorsque deux joueurs humains s'affrontent
+%Game between 2 human players.
 jeuHumainHumain([Marche,Bourse,Trader,ResJ1,ResJ2],JoueurenCours):-
 	length(Marche, Res),
 	(Res > 2 -> jouer_coup([Marche,Bourse,Trader,ResJ1,ResJ2], JoueurenCours, NewP),
 	 change(JoueurenCours, NewJ), jeuHumainHumain(NewP, NewJ); gagnant([Marche,Bourse,Trader,ResJ1,ResJ2], G), nl, write('Le gagnant est le joueur '), write(G)).
 
 %jeuHumainOrdi(+Plateau, +Joueur)
-%Ce prédicat est utilisé lorsque un joueur humain et une IA s'affrontent
+%Game between a bot and a human player
 jeuHumainOrdi([Marche,Bourse,Trader,ResJ1,ResJ2],JoueurenCours):-
 	length(Marche, Res),
 	(Res>2 -> 
@@ -45,13 +45,13 @@ jeuHumainOrdi([Marche,Bourse,Trader,ResJ1,ResJ2],JoueurenCours):-
 
 %SECURITE DEPLACEMENT (1,2 ou 3)
 %liredeplacement(?Deplacement)
-%Ce prédicat est utilisé pour afficher le menu du déplacement du trader
+%Display the menu to choose a movement
 liredeplacement(X) :-
-	write('De combien voulez vous avancer ? (1,2,3)'), nl,
+	write('Chose your movement (1,2,3)'), nl,
 	read(X),
 	liredep(X), !.
 liredeplacement(X) :-
-	nl,write('ENTRE 1, 2 ET 3 !!!!!'),nl,
+	nl,write('1, 2 OR 3 !!!!!'),nl,
 	liredeplacement(X).
 
 liredep(1):-!.
@@ -60,12 +60,12 @@ liredep(3):-!.
 
 %SECURITE CHOIX MARCHANDISE (1 ou 2)
 %liremarchandise(?Deplacement)
-%Ce prédicat est utilisé pour afficher le menu de choix de la marchandise
+%Display the menu to choose the commodity
 liremarchandise(X) :-
-	write('Lequel voulez vous garder ? (1 ou 2)'),
+	write('Which one are you keeping ? (1 ou 2)'),
 	nl,read(X),
 	liremarch(X),!.
-liremarchandise(X) :- nl,write('ENTRE 1 ET 2 !!!!!'),nl,liremarchandise(X).
+liremarchandise(X) :- nl,write('1 OR 2 !!!!!'),nl,liremarchandise(X).
 
 liremarch(1):-!.
 liremarch(2):-!.
@@ -73,7 +73,7 @@ liremarch(2):-!.
 
 /*_____________________________ JOUER COUP _____________________________*/
 %jouer_coup(+Plateau, +Joueur, ?NouveauPlateau)
-%Ce prédicat s'occupe de la gestion d'un seul coup pour un joueur humain
+%Handle the move of a human player
 jouer_coup([Marche,Bourse,Trader,ResJ1,ResJ2],JoueurenCours, [NewM, NewB, TmpPos, NewJ1, NewJ2]):-
 	liredeplacement(Deplacement),
 	getPosTrader(Deplacement,[Marche,Bourse,Trader,ResJ1,ResJ2],NewPos),
@@ -81,24 +81,23 @@ jouer_coup([Marche,Bourse,Trader,ResJ1,ResJ2],JoueurenCours, [NewM, NewB, TmpPos
 	newMarcheBourseRes([Marche,Bourse,NewPos,ResJ1,ResJ2],[NewM,NewB,TmpPos,NewJ1,NewJ2], JoueurenCours),
 	affiche_plateau([NewM, NewB, TmpPos, NewJ1, NewJ2]).
 
-%Changement Joueur
 %change(+Joueur, ?AutreJoueur)
-%Ce prédicat renvoie la valeur de l'autre joueur
+%Change player
 change(1,2).
 change(2,1).
 
-%RENVOIE LA NOUVELLE POSITION TRADER
+%RETURN THE NEW POSITION OF THE TRADER
 %getPosTrader(+Deplacement, +Plateau, ?NouveauTrader)
-%Ce prédicat renvoie la nouvelle position du trader selon son emplacement et la taille du marché
+%return the new position of trader depending on its position and the size of the board game
 getPosTrader(D,[M,_,T,_,_],NewT):-
 	length(M,Len),
 	TmpT is D+T,
 	modulo(TmpT, Len, NewT).
 
-%RENVOIE NOUVEAU MARCHE,MET A JOUR RESERVE
+%RETURN NEW BOARD GAME, UPDATE THE STOCKPILE
 %newMarcheBourseRes(+Plateau, ?NouveauPlateau, +JoueurEnCours)
-%Ce prédicat récupère les voisins du trader, met à jour le marché, permet au joueur de choisir la marchandise, 
-%met à jour sa réserve ainsi que la bourse
+%Get the trader's neighbors, update the Board Game 
+%Update the Stockpile and the Stock Exchange
 newMarcheBourseRes([M,B,T,J1,J2], [NewM,NewB,NewT,NewJ1,J2], 1):-
 	getvoisins(M,T,VoisinG,VoisinD),
 	pop(VoisinG, VoisinD, M, NewM,M1,M2, Vide),
@@ -108,7 +107,6 @@ newMarcheBourseRes([M,B,T,J1,J2], [NewM,NewB,NewT,NewJ1,J2], 1):-
 	setValeurMarchandise(Vendue, B, NewB).
 
 %newMarcheBourseRes(+Plateau, ?NouveauPlateau, +JoueurEnCours)
-%Identique que le prédicat ci-dessus
 newMarcheBourseRes([M,B,T,J1,J2], [NewM,NewB,NewT,J1,NewJ2], 2):-
 	getvoisins(M,T,VoisinG,VoisinD),
 	pop(VoisinG, VoisinD, M, NewM,M1,M2, Vide),
@@ -118,7 +116,7 @@ newMarcheBourseRes([M,B,T,J1,J2], [NewM,NewB,NewT,J1,NewJ2], 2):-
 	setValeurMarchandise(Vendue,B,NewB).
 
 %getvoisins(+Marche, +Trader, ?IndexGauche, ?IndexDroit)
-%Ce prédicat renvoie l'index de la pile à gauche du trader ainsi que l'index de la pile à sa droite
+%Return the pile's index of the left and right side of the Trader's position
 getvoisins(M,T,TG,TD):-
 	length(M,Len),
 	(T==1 -> TmpGauche is Len;
@@ -127,24 +125,24 @@ getvoisins(M,T,TG,TD):-
 	TmpDroite is T+1,
 	modulo(TmpDroite, Len, TD).
 
-%Ajout a  la reserve du Joueur
+%Add to 
 %addReserve(+Choix, +Marchandise1, +Marchandise2, +ReserveJoueurEnCours, ?NouvelleReserveJoueur, ?MarchandiseJetee)
-%Ce prédicat ajoute à la réserve du joueur en cours la marchandise associée à son choix (1 ou 2) et renvoie la marchandise jetée
+%Update the current player's Stockpile and return the commodity thrown away
 addReserve(1,M1,M2,ResenCours,[M1|ResenCours],M2).
 addReserve(2,M1,M2,ResenCours,[M2|ResenCours],M1).
 
 %getValeurMarchandise(+Marchandise, +Bourse, ?ValeurMarchandise)
-%Recupere la valeur d'une marchandise M dans la bourse B
+%Get the commodity's value depending on the Stock Exchange
 getValeurMarchandise(M, [[M|[Q]]|_], Q):-!.
 getValeurMarchandise(M, [_|Q], V):- getValeurMarchandise(M, Q, V).
 
 %setValeurMarchandise(+Marchandise, +BourseActuelle, +Valeur, ?NouvelleBourse)
-%Modifie la valeur d'une marchandise dans la bourse
+%Decrease the value of a commodity in the Stock Exchange
 setValeurMarchandise(M, [[M|[OldV]]|Q2], [[M|[V]]|Q2]):- V is OldV-1,!.
 setValeurMarchandise(M, [T|Q],[T|B]):- setValeurMarchandise(M, Q, B),!.
 
 %modulo(+X, +Y, ?Z)
-% Réimplémentation du modulo de SWI-Prolog pour ne pas avoir un modulo = 0
+% Implementation of an adapted modulo
 modulo(6,3,1).
 module(4,2,1).
 modulo(X,Y,Z):- X > Y, Z is X mod Y,!.
@@ -154,14 +152,14 @@ modulo(X,_,X).
 
 /*____________________ AFFICHAGE PLATEAU DE JEU _______________________*/
 %affiche_bourse(+Bourse)
-%Affichage de la bourse en cours
+%Display the current Stock Exchange
 affiche_bourse(Bourse) :-
 	nl,
 	nl, write('---------------- Bourse ----------------'),
 	affiche_marchandise(Bourse).
 
 %affiche_marchandise(+Bourse)
-%Affichage de la marchandise en cours ainsi que sa valeur
+%Display the current commodity and its value
 affiche_marchandise([]).
 affiche_marchandise([[M|[V]]|Q]) :-
 	nl,
@@ -171,7 +169,7 @@ affiche_marchandise([[M|[V]]|Q]) :-
 	affiche_marchandise(Q).
 
 %affiche_pile(+Marche, +Trader)
-%Affichage du marché avec la position du trader
+%Display the Board Game with the Trader's position
 affiche_pile(Marche, Trader) :-
 	nl,
 	write('----------------- Piles de jeu -----------------'),nl,
@@ -193,11 +191,11 @@ affiche_pile([P|L], Trader, Ct) :-
 	affiche_pile(L, Trader, Tmp).
 
 %affiche_reserve(+Joueur, +Reserve)
-%Affichage d'une réserve d'un joueur
+%Display a player's Stockpile
 affiche_reserve(X,L):-nl,write('Reserve Joueur'),write(X),write(' : '), write(L),nl.
 
 %affiche_plateau(+Plateau)
-%Affichage du plateau
+%Display the Boardgame
 affiche_plateau([Marche,Bourse,Trader,Res1,Res2]) :-
 	nl,write('============== PLATEAU DE JEU ==================='),nl, write('================================================='),nl,
 	affiche_pile(Marche, Trader),
@@ -214,14 +212,13 @@ affiche_plateau([Marche,Bourse,Trader,Res1,Res2]) :-
 /*_______________________FONCTIONS DE SERVICE__________________________*/
 
 
-%retire la tete de la pile M de rang N1 et N2 et l'affiche
+%Pop the piles ranked N1 & N2 from the Boardgame M
 %pop(+IndexGauche, +IndexDroite, +Marche, ?NouveauMarche, ?MarchandiseGauche, ?MarchandiseDroite, ?Vide)
-%Enlève les marchandises des piles à l'index IndexGauche et IndexDroite du marché, renvoie le nouveau marché,
-% renvoie les marchandises enlevées
-%Vide = 1 si une pile a été supprimée (car vide)
-%Vide = 0 sinon
+%Return the new Boardgame and the commodity thrown away
+%Vide = 1 if one of the pile was deleted (because empty)
+%Vide = 0 otherwise
 pop(N1,N2,M,NewM,T1,T2, Vide):-
-	write('Marchandises enlevées:'),nl,
+	write('Commodities thrown away:'),nl,
 	nth1(N1, M, [T1|Q1]),
 	write('1)'),
 	write(T1),
@@ -240,44 +237,43 @@ pop(N1,N2,M,NewM,T1,T2, Vide):-
 	!.
 
 %replace(+Liste,+Index,+NouvelleValeur, ?NouvelleListe)
-remplace l'éement de rang Index de la liste Liste par NouvelleValeur
+Replace the "Index" ranked element of the list "Liste" by "NouvelleValeur"
 replace([_|Q], 1, X, [X|Q]).
 replace([T|Q], I, X, [T|R]):- I > 0, NI is I-1, replace(Q, NI, X, R), !.
 replace(L, _, _, L).
 
-/*_____________INITIALISATION PLATEAU _____________*/
+/*_____________BOARDGAME INITIALISATION _____________*/
 
 %nth1RandDelete(?Element, +Liste, ?NouvelleListe)
-%Le prédicat choisit un élement aléatoire dans la liste Liste, renvoie dans Element et renvoie la nouvelle liste dans
-%NouvelleListe
+%Chooses a random element in the list "Liste", returns it in "Element" and return the new list in "NouvelleListe"
 nth1RandDelete(Y, [X|R], Z):-
 	length([X|R], Long),
 	%Random entre deux entiers compris
 	random_between(1, Long, Tmp),
-	%RÃ©cupÃ¨re l'Ã©lÃ©ment Ã  l'index Tmp, et rÃ©cupÃ¨re l'Ã©lement dans Y
+	%RÃƒÂ©cupÃƒÂ¨re l'ÃƒÂ©lÃƒÂ©ment ÃƒÂ  l'index Tmp, et rÃƒÂ©cupÃƒÂ¨re l'ÃƒÂ©lement dans Y
 	nth1(Tmp, [X|R], Y),
-	%Supprime l'Ã©lÃ©ment Ã  l'index Tmp, et mets la liste finale dans Z
+	%Supprime l'ÃƒÂ©lÃƒÂ©ment ÃƒÂ  l'index Tmp, et mets la liste finale dans Z
 	nth1(Tmp, [X|R], _, Z).
 
-%Initialisation de la bourse
+%Stock Exchange Initialisation
 initBourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]]).
 
 %initTrader(?Position)
-%Initialise la position du trader
+%Trader's position initialision
 initTrader(X) :-
 	random(1,9, X).
 
-%Initialise la liste de marchandises disponibles (chaque marchandise est répétée 6 fois)
+%Commodities' list initialision
 initLimites([ble, ble, ble, ble, ble, ble, riz, riz, riz, riz, riz, riz, cacao, cacao, cacao, cacao, cacao, cacao,
 			cafe, cafe, cafe, cafe, cafe, cafe, sucre, sucre, sucre, sucre, sucre, sucre, mais, mais, mais, mais, mais, mais]).
 
 %initMarchandise(?Marchandise, +ListeMarchandises, ?NouvelleListe)
-%M = marchandise, [Y|R] = Liste marchandise modif, Z = liste marchandise aprÃ¨s modif
+%M = marchandise, [Y|R] = Liste marchandise modif, Z = liste marchandise aprÃƒÂ¨s modif
 initMarchandise(M, [Y|R], Z) :-
 	nth1RandDelete(M, [Y|R], Z).
 
 %initPile(?Pile, +ListeMarchandises, ?NouvelleListeMarchandise)
-%Initialisation de la Pile, Final est la liste modifiÃ©e (sans les marchandises ajoutÃ©es)
+%Initialisation de la Pile, Final est la liste modifiÃƒÂ©e (sans les marchandises ajoutÃƒÂ©es)
 %Une Pile est liste de 4 marchandises
 initPile([A, B, C, D], X, Final):-
 	initMarchandise(A, X, Z),
@@ -286,7 +282,7 @@ initPile([A, B, C, D], X, Final):-
 	initMarchandise(D, M, Final).
 
 %initMarche(?Marche)
-%Initilisation du marchÃ© (Une liste qui reprÃ©sente 9 piles))
+%Initilisation du marchÃƒÂ© (Une liste qui reprÃƒÂ©sente 9 piles))
 initMarche([A, B, C, D, E, F, G, H, I]) :-
 	initLimites(X),
 	initPile(A, X, J),
@@ -309,19 +305,19 @@ initTrader(Trader).
 /*_____________FIN DU JEU_____________*/
 %Calcul somme J1
 %somme(+Bourse, +ReserveJoueur, ?Score)
-%Le prédicat calcule le score d'un joueur selon sa réserve et la bourse en cours
+%Calculated the player score depending on his StockPile and the Stock Exchange
 somme(_,[],0):-!.
 somme(B,[T|Q],N):-getValeurMarchandise(T,B,R), somme(B,Q,N1),  N is N1+R.
 
 %gagnant(+Plateau, ?JoueurGagnant)
-%Le prédicat détermine le gagnant du jeu selon leur score
+%Finds out the game's winner depending on their score
 gagnant([_,B,_,J1,J2],1):-somme(B,J1,SJ1),somme(B,J2,SJ2),SJ1>SJ2,!.
 gagnant([_,B,_,J1,J2],2):-somme(B,J1,SJ1),somme(B,J2,SJ2),SJ2>SJ1,!.
 
 /*_____________INTEL ARTIFICIELLE_____________*/
 
 %coups_possibles(+Plateau, +Deplacement, +JoueurEnCours, ?ListeCoupsPossibles)
-%Ce prédicat détermine tous les coups possibles selon un déplacement et le plateau actuel
+%Ce prÃ©dicat dÃ©termine tous les coups possibles selon un dÃ©placement et le plateau actuel
 coups_possibles(_, 0, _, []).
 coups_possibles([Marche,Bourse,Trader,ResJ1,ResJ2],Dep, Joueur,[[Joueur, Dep, MarchG, MarchD]|[[Joueur, Dep, MarchD, MarchG]|Liste]]):-
 	getPosTrader(Dep, [Marche,Bourse,Trader,ResJ1,ResJ2],NewTrader),
@@ -331,14 +327,14 @@ coups_possibles([Marche,Bourse,Trader,ResJ1,ResJ2],Dep, Joueur,[[Joueur, Dep, Ma
 	coups_possibles([Marche,Bourse,Trader,ResJ1,ResJ2],NewDep,Joueur,Liste),!.
 
 %getMarchandise(+IndexGauche, +IndexDroite, +Marche, ?MarchandiseGauche, ?MarchandiseDroite)
-%Ce prédicat récupère les marchandises à la tête des piles à l'index IndexGauche et IndexDroit
+%Ce prÃ©dicat rÃ©cupÃ¨re les marchandises Ã  la tÃªte des piles Ã  l'index IndexGauche et IndexDroit
 getMarchandise(IndexG, IndexD, Marche, MarchG, MarchD) :-
         nth1(IndexG, Marche, [MarchG|_]),
 	nth1(IndexD, Marche, [MarchD|_]).
 
 %pop_voisins(+Marche, ?NouveauMarche, +IndexVoisinGauche, +IndexVoisinDroite, ?Vide)
-%Ce prédicat enlève la tête des piles IndexVoisinGauche et IndexVoisinDroite et renvoie le nouveau marché
-%Vide = 1 si une pile est supprimée (car vide)
+%Ce prÃ©dicat enlÃ¨ve la tÃªte des piles IndexVoisinGauche et IndexVoisinDroite et renvoie le nouveau marchÃ©
+%Vide = 1 si une pile est supprimÃ©e (car vide)
 %Vide = 0 sinon
 pop_voisins(Marche, NewM, Voisin1, Voisin2, Vide) :-
 	nth1(Voisin1, Marche, [_|Q1]),
@@ -352,7 +348,7 @@ pop_voisins(Marche, NewM, Voisin1, Voisin2, Vide) :-
         (TmpL == OldL -> Vide is 0; Vide is 1),!.
 
 %simuler_coup(+Plateau, +CoupEnCours, ?NouveauMarche)
-%Ce prédicat simule le coup CoupEnCours et renvoie un NouveauMarche
+%Ce prÃ©dicat simule le coup CoupEnCours et renvoie un NouveauMarche
 simuler_coup([Marche,Bourse,Trader,J1,J2], [1, Deplacement, Gardee, Jetee], [NewM,NewB,NewT,NewJ1,J2]) :-
 		%write('Trader: '), write(Trader), nl,
         getPosTrader(Deplacement, [Marche, Bourse, Trader, J1, J2], NewPos),
@@ -372,11 +368,11 @@ simuler_coup([Marche,Bourse,Trader,J1,J2], [2, Deplacement, Gardee, Jetee], [New
 		ajouterReserve(Gardee,J2,NewJ2).
 
 %ajouterReserve(+Marchandise, +ReserveduJoueur, ?NouvelleReserve)
-%Ce prédicat ajoute la marchandise à la réserve
+%Ce prÃ©dicat ajoute la marchandise Ã  la rÃ©serve
 ajouterReserve(March, Res,[March|Res]).
 
 %iafinal(+Plateau, +JoueurEnCours, ?NouveauPlateau)
-%Ce prédicat est le début de l'IA et fait un coup avec l'IA
+%Ce prÃ©dicat est le dÃ©but de l'IA et fait un coup avec l'IA
 iafinal([Marche,Bourse,Trader,ResJ1,ResJ2], Joueur, NewPlateau):-
 	ia([Marche,Bourse,Trader,ResJ1,ResJ2],Joueur,NewPlateau),
 	affiche_plateau(NewPlateau).
@@ -391,9 +387,9 @@ ia([Marche,Bourse,Trader,ResJ1,ResJ2], Joueur, PlateauTmp):-
 	%on joue le meilleurcoup trouve par main
 	simuler_coup([Marche,Bourse,Trader,ResJ1,ResJ2], MeilleurCoup,PlateauTmp).
 
-%Déroulement d'un seul coup
+%DÃ©roulement d'un seul coup
 %main(+Plateau, +JoueurEnCours, +ListeCoupsPossibles, ?MeilleurCoup, ?MeilleurScore)
-%Ce prédicat déroule un seul coup et calcule le meilleur coup possible
+%Ce prÃ©dicat dÃ©roule un seul coup et calcule le meilleur coup possible
 main(_,_,[],[],0).
 main(Plateau, Joueur, [T|Q],MeilleurCoup):- main(Plateau, Joueur, [T|Q], MeilleurCoup, MeilleurScore).
 main(Plateau, Joueur, [T|Q], MeilleurCoup, MeilleurScore):-
@@ -415,7 +411,7 @@ main(Plateau, Joueur, [T|Q], MeilleurCoup, MeilleurScore):-
 		MeilleurScore is Score, MeilleurCoup = T,!).
 
 %scoreAdverse(+JoueurEnCours, +Plateau, ?ScoreAdverse)
-%Ce prédicat retourne le score du meilleur coup du joueur adverse
+%Ce prÃ©dicat retourne le score du meilleur coup du joueur adverse
 scoreAdverse(Joueur,[Marche,Bourse,Trader,ResJ1,ResJ2],Res):-
 	change(Joueur, NewJoueur),
 	coups_possibles([Marche,Bourse,Trader,ResJ1,ResJ2],3, NewJoueur,Liste),
@@ -423,7 +419,7 @@ scoreAdverse(Joueur,[Marche,Bourse,Trader,ResJ1,ResJ2],Res):-
 	max_member(Res, ListeMax).
 
 %meilleurscore(+Plateau, +JoueurEnCours, +ListeCoupsPossibles, ?ListeScores)
-%Ce prédicat calcule le meilleur score du joueur adverse selon les coups possibles
+%Ce prÃ©dicat calcule le meilleur score du joueur adverse selon les coups possibles
 meilleurscore(_, _, [], []).
 meilleurscore([Marche,Bourse,Trader,ResJ1,ResJ2], Joueur, [T|Q],[Score|Liste]):-
 	%write('Coup possible'), write(T),nl,
@@ -434,7 +430,7 @@ meilleurscore([Marche,Bourse,Trader,ResJ1,ResJ2], Joueur, [T|Q],[Score|Liste]):-
 	scoreTemporaire(NewPlateau, Joueur, Score).
 
 %scoreTemporaire(+Plateau, +JoueurEnCours, ?Score)
-%Ce prédicat calcule le score temporaire du joueur actuel selon sa réserve
+%Ce prÃ©dicat calcule le score temporaire du joueur actuel selon sa rÃ©serve
 scoreTemporaire([_,Bourse,_,J1,_], 1, N):-
         somme(Bourse, J1, N),!.
 scoreTemporaire([_, Bourse, _, _, J2], 2, N):-
